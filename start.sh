2bd7a3366f95696330c85ee123c7336e19160ae1
COLEN_ <718399822@qq.com>
2019-12-22 01:10:47 +0800

    fix

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 CWD=$(cd "$(dirname "$0")" && pwd)
+ENV="dev"
+PROD_BACKUP_DIR="/home/docker/docker/data/"
 
 mkdir -p ${CWD}/data/mysql
+mkdir -p ${CWD}/data/redis
+mkdir -p ${CWD}/data/rabbitmq
 
 #清空Mysql数据
 sudo rm -rf ${CWD}/data/mysql/*
