d593eafa25c9f391707ea0eb57c59014e756ea13
少喝肥宅水 <718399822@qq.com>
2020-05-25 14:23:24 +0800

    fix

--- entrypoint/mongo.sh
+++ entrypoint/mongo.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
+
 #设置时区
 cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 mv /etc/apt/sources.list /etc/apt/sources.list.bak
-echo "deb http://mirrors.aliyun.com/debian jessie main contrib non-free" >/etc/apt/sources.list
-echo "deb-src http://mirrors.aliyun.com/debian jessie main contrib non-free" >>/etc/apt/sources.list
-echo "deb http://mirrors.aliyun.com/debian jessie-updates main contrib non-free" >>/etc/apt/sources.list
-echo "deb-src http://mirrors.aliyun.com/debian jessie-updates main contrib non-free" >>/etc/apt/sources.list
-echo "deb http://mirrors.aliyun.com/debian-security jessie/updates main contrib non-free" >>/etc/apt/sources.list
-echo "deb-src http://mirrors.aliyun.com/debian-security jessie/updates main contrib non-free" >>/etc/apt/sources.list
+echo "deb http://mirrors.aliyun.com/debian/ stretch main non-free contrib" >/etc/apt/sources.list
+echo "deb http://mirrors.aliyun.com/debian/ stretch-updates main non-free contrib" >>/etc/apt/sources.list
+echo "deb http://mirrors.aliyun.com/debian/ stretch-backports main non-free contrib" >>/etc/apt/sources.list
+echo "deb-src http://mirrors.aliyun.com/debian/ stretch main non-free contrib" >>/etc/apt/sources.list
+echo "deb-src http://mirrors.aliyun.com/debian/ stretch-updates main non-free contrib" >>/etc/apt/sources.list
+echo "deb-src http://mirrors.aliyun.com/debian/ stretch-backports main non-free contrib" >>/etc/apt/sources.list
+echo "deb http://mirrors.aliyun.com/debian-security/ stretch/updates main non-free contrib" >>/etc/apt/sources.list
+echo "deb-src http://mirrors.aliyun.com/debian-security/ stretch/updates main non-free contrib" >>/etc/apt/sources.list
+
 openssl rand -base64 741 >mongodb-keyfile
 chmod 600 mongodb-keyfile
 chown 999 mongodb-keyfile
+
 apt update
 dpkg --configure -a
-apt install procps vim iproute2 curl wget netcat htop jq net-tools sudo -y
+apt install procps vim iproute2 telnet curl wget netcat htop jq net-tools sudo -y
 
 mongod -f /data/mongo/config/my.cnf &
 
